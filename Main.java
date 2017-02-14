364c3e2c553e33992d0e5fcaabeb94167e16d3fc
kfiravr2 <kfira@jfrog.com>
2017-02-14 17:10:00 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int num = input.nextInt();
+        System.out.println("num" + num);
+    }
+}
